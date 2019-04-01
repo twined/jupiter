b64251797d278694d381009b6d9edf790228897c
T. Mjoen <trond@twined.net>
2019-04-01 22:43:03 +0200

    Add objectfit fill

--- src/HeroSlider/index.js
+++ src/HeroSlider/index.js
@@ -16,6 +16,15 @@ import _debounce from 'lodash.debounce'
 // eslint-disable-next-line no-unused-vars
 const plugins = [CSSPlugin]
 
+if ('objectFit' in document.documentElement.style === false) {
+  document.addEventListener('DOMContentLoaded', function () {
+    Array.prototype.forEach.call(document.querySelectorAll('.hero-bg img'), function (image) {
+      (image.runtimeStyle || image.style).background = 'url("' + image.src + '") no-repeat 50%/' + (image.currentStyle ? image.currentStyle['object-fit'] : image.getAttribute('data-object-fit'))
+      image.src = 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'' + image.width + '\' height=\'' + image.height + '\'%3E%3C/svg%3E'
+    })
+  })
+}
+
 const DEFAULT_OPTIONS = {
   /* time between slides */
   interval: 4.2,
