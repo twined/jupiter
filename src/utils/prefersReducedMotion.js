356a53b67d01f3c8fe566ba88cc0a7e6e79e579b
T. Mjoen <trond@twined.net>
2020-01-24 13:05:45 +0100

    Force false

--- src/utils/prefersReducedMotion.js
+++ src/utils/prefersReducedMotion.js
@@ -2,15 +2,5 @@
  * Checks if OS prefers reduced motion
  */
 export default function () {
-  if (!window.matchMedia) {
-    return false
-  }
-
-  const matchMediaObj = window.matchMedia('(prefers-reduced-motion: reduce)')
-
-  if (matchMediaObj) {
-    return matchMediaObj.matches
-  }
-
   return false
 }
